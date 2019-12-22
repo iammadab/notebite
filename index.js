bc9c286afe9e4469117a37fc69e55dbc472ac99a
iammadab <iammadab@gmail.com>
2019-12-22 09:58:17 +0100

    notes -> file

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const {
 cli.use({
 	commands: {
 		folder: ["locator"],
-		note: ["locator"],
+		file: ["locator"],
 		list: ["locator"],
 		open: ["locator"]
 	}
 })
 
 cli.action("folder", passArg(createFolder))
-cli.action("note", passArg(createNote))
+cli.action("file", passArg(createNote))
 cli.action("list", passArg(listFolder))
 cli.action("open", passArg(openNote))
 
